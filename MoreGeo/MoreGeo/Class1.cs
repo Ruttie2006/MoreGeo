8c743731d2a6e8b2f719e60fd1521ebffaf43b94
Ruttie <80394237+Ruttie2006@users.noreply.github.com>
2021-08-03 12:25:32 +0200

    Initial commit

---
+++ MoreGeo/MoreGeo/Class1.cs
@@ -0,0 +1,30 @@
+﻿using Modding;
+
+namespace MoreGeo
+{
+    class MoreGeo : Mod
+    {
+        private int Multiplier = 2;
+        private int GeoTarget;
+
+        private string VerNumber;
+
+        public MoreGeo() : base("MoreGeo Multiplier ")
+        {
+            VerNumber = Multiplier.ToString();
+        }
+
+        public override string GetVersion() => VerNumber;
+
+        public override void Initialize()
+        {
+            On.HeroController.AddGeo += AddGeo;
+        }
+
+        private void AddGeo(On.HeroController.orig_AddGeo orig, HeroController self, int amount)
+        {
+            GeoTarget = amount * Multiplier;
+            orig(self, GeoTarget);
+        }
+    }
+}
