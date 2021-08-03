036d60c0791ef527191117c8699d8586d489b552
Ruttie <80394237+Ruttie2006@users.noreply.github.com>
2021-08-03 12:48:53 +0200

    GlobalSettings

--- MoreGeo/MoreGeo/Class1.cs
+++ MoreGeo/MoreGeo/Class1.cs
@@ -1,17 +1,18 @@
 ﻿using Modding;
+using GlobalSettings;
 
 namespace MoreGeo
 {
     class MoreGeo : Mod
     {
-        private int Multiplier = 2;
         private int GeoTarget;
-
         private string VerNumber;
+
+        GlobalSettingsClass Settings = new GlobalSettingsClass();
 
         public MoreGeo() : base("MoreGeo Multiplier ")
         {
-            VerNumber = Multiplier.ToString();
+            VerNumber = Settings.Multiplier.ToString();
         }
 
         public override string GetVersion() => VerNumber;
@@ -23,7 +24,7 @@ namespace MoreGeo
 
         private void AddGeo(On.HeroController.orig_AddGeo orig, HeroController self, int amount)
         {
-            GeoTarget = amount * Multiplier;
+            GeoTarget = amount * Settings.Multiplier;
             orig(self, GeoTarget);
         }
     }
---
+++ MoreGeo/MoreGeo/GlobalSettingsClass.cs
@@ -0,0 +1,9 @@
+﻿using Modding;
+
+namespace GlobalSettings
+{
+	public class GlobalSettingsClass : ModSettings
+	{
+		public int Multiplier = 2;
+	}
+}
